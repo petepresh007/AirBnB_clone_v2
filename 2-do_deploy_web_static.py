d2228e931128ddf5dfd17ca6f3062742ba2735c7
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-03-11 03:13:48 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from fabric.api import run, put, env
 from os.path import exists
 env.hosts = ['54.87.179.70', '54.144.144.220']
 
@@ -13,14 +13,19 @@ def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
-    file_n = archive_path.split("/")[-1]
-    no_ext = file_n.split(".")[0]
-    path = "/data/web_static/releases/"
-    put(archive_path, '/tmp/')
-    run('sudo mkdir -p {}{}/'.format(path, no_ext))
-    run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-    run('sudo rm /tmp/{}'.format(file_n))
-    run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-    run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
-    run('sudo rm -rf /data/web_static/current')
-    run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_n))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
